60768193db5e66db77de9e8f5706abfb0b94c145
sagarbhandari <sagarvandari1@gmail.com>
2022-01-12 01:26:25 +0545

    Performing authentication and sorting

--- todo.rb
+++ todo.rb
@@ -4,37 +4,66 @@
 require 'mysql2'
 require 'date'
 require '/home/sagar/Todo_Application/todo_class'
-# class TODO
 
 a = TODO.new
-puts "\n"
-loop do
-  option = a.help
-  case option.to_i
-  when 1
-    puts 'Enter a todo'
-    todo = gets.chomp
-    puts 'Enter a date to be completed'
-    date = gets.chomp
-    a.add_todos(todo, date)
-  when 2
-    a.remaining_todos
-  when 3
-    puts 'Enter a todos_id to be deleted'
-    a.all_todos
-    id = gets.chomp
-    a.delete_todos(id)
-  when 4
-    puts 'Enter a todos_id of completed todo'
-    id = gets.chomp
-    a.complete_todos(id)
-  when 5
-    a.show_statistics
-  else
-    puts 'inavlid options'
+auth = a.authentication
+if auth == 'success'
+  loop do
+    option = a.help
+    case option.to_i
+    when 1
+      begin
+        a.add_todos
+      rescue StandardError => e
+        puts e.message
+        retry
+      end
+    when 2
+      begin
+        a.remaining_todos
+      rescue StandardError => e
+        puts e.message
+        retry
+      end
+    when 3
+      begin
+        a.all_todos
+        a.delete_todos
+      rescue StandardError => e
+        puts e.message
+        retry
+      end
+    when 4
+      begin
+        puts 'Enter a todos_id of completed todo'
+        id = gets.chomp
+        a.complete_todos(id)
+      rescue StandardError => e
+        puts e.message
+        retry
+      end
+    when 5
+      begin
+        a.show_statistics
+      rescue StandardError => e
+        puts e.message
+        retry
+      end
+    when 6
+      begin
+        a.add_user
+      rescue StandardError => e
+        puts e.message
+        retry
+      end
+    else
+      puts 'inavlid options'
+    end
+    puts 'Do you want to continue Y/N'
+    c = gets.chomp.upcase
+    break if c != 'Y'
   end
-  puts 'Do you want to continue Y/N'
-  c = gets.chomp.upcase
-  break if c != 'Y'
+else
+  puts 'Thanks for using app'
 end
-puts 'Thanks for Use app'
+puts 'Thanks for using app'
--- todo_class.rb
+++ todo_class.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 # class TODO
 class TODO
+  attr_accessor :client, :users, :a
+
   def initialize
     @client = Mysql2::Client.new(
       host: 'localhost',
@@ -7,52 +11,139 @@ class TODO
       username: 'root',
       password: 'password'
     )
+    @users = client.query('select * from login')
   end
 
-  def add_todos(todo, date)
-    date1 = Date.parse(date)
-    now = Time.now.to_date
-    remaining_days = (date1.mjd - now.mjd).to_i
-    @client.query("insert into todo_tbl(todo,date_to_be_completed,remaining_days)
-          values('#{todo}','#{date1}',#{remaining_days});")
+  def authentication
+    attempt = 3
+    check = 'success'
+    while attempt.positive?
+      puts 'Enter login credintials'
+      print "Username:\t"
+      username = gets.chomp.to_s
+      print "Password:\t"
+      password = gets.chomp.to_s
+      users.each do |user|
+        check = 'success'
+        break if user['username'] == username && user['password'] == password
+
+        check = 'wrong'
+      end
+      puts
+      case check
+      when 'success'
+        puts 'You are loged in'
+      when 'wrong'
+        puts 'You enter the wrong credintials'
+      end
+      break if check == 'success'
+
+      attempt -= 1
+
+    end
+    case check
+    when 'success'
+      check
+    when 'wrong'
+      puts 'You enter the wrong passoword 3 times'
+      print 'Do you want to continue Y/N  '
+      c = gets.chomp.upcase
+      if c == 'Y'
+        authentication
+      else
+        check
+      end
+    end
+  end
+
+  def add_todos
+    date_lambda = lambda {
+      puts 'Enter a date to be completed'
+      date = gets.chomp
+      date = Date.parse(date)
+      now = Time.now.to_date
+      remaining_days = (date.mjd - now.mjd).to_i
+      puts date, remaining_days
+      if remaining_days.negative?
+        puts 'You enter wrong date ie already passed'
+        puts 'Re-enter date'
+        date = date_lambda.call
+        return date
+      end
+
+      return date, remaining_days
+    }
+    puts 'Enter a todo'
+    todo = gets.chomp
+    date = date_lambda.call
+    date_completed = date[0]
+    remaining_days = date[1]
+
+    client.query("insert into todo_tbl(todo,date_to_be_completed,remaining_days)
+          values('#{todo}','#{date_completed}',#{remaining_days});")
     puts 'TODOS Added'
   end
 
+  def add_user
+    print 'Enter username: '
+    username = gets.chomp
+    print 'Enter the password: '
+    password = gets.chomp
+    client.query("insert into login(username,password)
+    values('#{username}','#{password}')")
+    puts 'User added'
+    self.users = client.query('select * from login')
+  end
+
   def complete_todos(todo_id)
-    @client.query("update todo_tbl
+    client.query("update todo_tbl
       set status = 'Completed'
       where todo_id = #{todo_id};")
   end
 
-  def delete_todos(todo_id)
-    @client.query("delete from todo_tbl where todo_id=#{todo_id}")
+  def delete_todos
+    a = []
+    puts 'Enter a todos_id to be deleted'
+    id = gets.chomp.to_i
+    all_id = client.query('select todo_id from todo_tbl')
+    all_id.each do |ids|
+      a.push(ids['todo_id'])
+    end
+    if a.include?(id)
+      client.query("delete from todo_tbl where todo_id=#{id}")
+      puts 'TODOS deleted'
+    else
+      puts "You enter the wrong todo id\nplease enter new one"
+      delete_todos
+    end
   end
 
   def show_statistics
-    a = @client.query("select count(*) as 'total' from todo_tbl where status='Remaining';")
+    a = client.query("select count(*) as 'total' from todo_tbl where status='Remaining';")
     a.each do |i|
-      puts "Remaining todos is #{i[:total]}"
+      puts "Remaining todos is #{i['total']}"
     end
-    a = @client.query("select count(*) as 'total' from todo_tbl where status='Completed';")
+    a = client.query("select count(*) as 'total' from todo_tbl where status='Completed';")
     a.each do |i|
-      puts "Completed todos is #{i[:total]}"
+      puts "Completed todos is #{i['total']}"
     end
   end
 
   def help
-    puts %(
+    print %(
         Select :-
           1. Add a new todo
           2. Show remaining todos
           3. Delete a todo
           4. Complete a todo
           5. Statistics
+          6.Add User
     Enter the Options:-)
     gets.chomp
   end
 
   def all_todos
-    todos = @client.query('select * from todo_tbl')
+    todos = client.query('select * from todo_tbl')
     puts('Todo_id Todos Date to be completed Remaining Days')
     todos.each do |i|
       # puts i['task_id']
@@ -61,7 +152,7 @@ class TODO
   end
 
   def remaining_todos
-    todos = @client.query("select * from todo_tbl where status='Remaining'")
+    todos = client.query('select * from todo_tbl where status="Remaining" order by remaining_days ASC')
     todos.each do |i|
       # puts i['task_id']
       puts "#{i['todo_id']}\t #{i['todo']}\t#{i['date_to_be_completed']}\t#{i['remaining_days']} \t #{i['status']}"
